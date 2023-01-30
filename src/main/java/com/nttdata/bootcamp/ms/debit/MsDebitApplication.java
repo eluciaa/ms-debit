0fa82c42953b9d7288e67b06761ff7ab1b4cc8fa
evillarv <esthervillarv@gmail.com>
2023-01-30 09:41:43 -0500

    first upload

---
+++ src/main/java/com/nttdata/bootcamp/ms/debit/MsDebitApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.ms.debit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDebitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDebitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/ms/debit/MsDebitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.ms.debit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDebitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
